aec9cc8f84ab02b21c754229e78c4d291fb47d8b
abhinav <abhinav@ubuntu18.myguest.virtualbox.org>
2022-11-14 21:20:21 +0530

    variables

---
+++ 02-variables/samples-type.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 8.9;
+var fname = "Abhinav";
+var lname = "Kesari";
+console.log(found);
+console.log(grade);
+console.log(fname + " " + lname);
+console.log("Hi " + fname + " " + lname);
---
+++ 02-variables/samples-type.ts
@@ -0,0 +1,11 @@
+let found : boolean = true;
+let grade : number = 8.9;
+let fname : string = "Abhinav";
+let lname : string = "Kesari";
+
+console.log(found);
+console.log(grade);
+console.log(fname + " " + lname);
+
+//template string
+console.log(`Hi ${fname} ${lname}`);
